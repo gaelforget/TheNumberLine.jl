4e10b48e0aacb2149b3259134b368dce1604ba8a
gaelforget <gforget@mit.edu>
2022-06-11 12:34:05 -0400

    🐛 improve histogram template

--- src/histogram_template.jl
+++ src/histogram_template.jl
@@ -14,16 +14,19 @@ fig=make_hist()
 save_hist(fig)
 ```
 """
-function make_hist()
-        f=Figure()
-        a=Axis(f[1,1],xticks=collect(-10.0:10.0),yticks=collect(0.0:10.0),
+function make_hist(;max_value=5.0,max_count=5,aspect_ratio=2.0)
+        f=Figure(resolution = (aspect_ratio*800, 800), fontsize=32.0)
+        a=Axis(f[1,1],xticks=collect(-max_value:max_value),yticks=collect(0.0:max_count),
                 xgridcolor=:black,ygridcolor=:black,xgridwidth=0.5,ygridwidth=0.5)
-        lines!(a,[0.0,0.0],[0.0,10.0],color=:red)
-        xlims!(a,(-10.0,10.0))
-        ylims!(a,(0.0,10.0))
+        lines!(a,[0.0,0.0],[0.0,max_count],color=:red,linewidth=2.0)
+        xlims!(a,(-max_value,max_value))
+        ylims!(a,(0.0,max_count))
         f
 end
 
-save_hist(fig::Makie.Figure;file=joinpath(tempdir(),"histogram_template.png")) = save(file,fig) 
+function save_hist(fig::Makie.Figure;file=joinpath(tempdir(),"histogram_template.png")) 
+        save(file,fig) 
+        file
+end
 
 end
